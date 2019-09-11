3668a64113e3f6b4122f80d83b7c18b4828af88e
Anselmoo <Anselm.Hahn@gmail.com>
2019-09-11 11:36:41 +0200

    Finished the comments of the sklsetups-modul

--- pyinsights/sklsetups.py
+++ pyinsights/sklsetups.py
@@ -1,17 +1,43 @@
-__all__ = ['feature_sort','alphas','elements','get_x_train']
+__all__ = ['feature_sort','alphas','elements', 'get_X_train']
 
 import numpy as np
 def feature_sort(features, index):
     """
-
-    1D-list :param features: with str, int, float, boal
-    1D-list :param index: int-list for sorting/ ordering
-    :return:
+    Sorts the features according the index-list
+    
+    Parameters
+    ----------
+    features: list
+        with str, int, float, boal elements
+    index: int-list 
+    for sorting/ re-ordering
+    
+    Returns
+    ----------
+    
+    A sorted feature-list       
     """
     return [features[i] for i in index]
 
 def alphas(start=0.05,stop=5.00,step=0.05):
-
+    """
+    Return evenly spaced values within a given interval with fixed thresholf of 0.001 minimum stepsize
+ 
+    Parameters
+    ----------
+    start : number, optional
+        Start of interval
+            
+    stop : number
+        End of interval
+    step : number
+        Incrimiment of the interval
+       
+    Returns
+    -------
+    arange : ndarray
+        Array of evenly spaced values.
+    """
     if start >= 0.001 and stop >= 0.001 and step >= 0.001:
         return np.arange(start=start, stop=stop, step=step)
     else:
@@ -20,17 +46,36 @@ def alphas(start=0.05,stop=5.00,step=0.05):
         return np.arange(start=0.05,stop=5.0,step=0.05)
 
 def elements(el):
+    """
+    Return evenly spaced values starting from 0 within a given interval of 1 for the maximum numbers of elements (el)
+    """
     return np.arange(len(el))
 
-def get_x_train(data,index_name=['competitorname', 'winpercent']):
+def get_X_train(data, index_name):
     """
-    2D-pandas-obj :param data: pandas-obs (numpy.ndarray)
-    1D-list :param index_name: expresions to exculde
-    1D-list, 2D-pandas :return: feature-list and 2D-pandas-object without the index_names
+    Import the pandas-data and select and sliced it according the column-index-list
+
+    Parameters
+    ----------
+
+    data: ndarray
+        Pandas data-stream
+    index_name: str-list
+        Columns-Names for removing columns out the data-stream
+
+    Returns
+    ----------
+
+    features: str-list
+        feature-list of the not-selected column-names
+    X_train: ndarray
+        pandas.dataframe for X-dataset
+
     """
+
     features = [i for i in list(data.columns.values) if i not in index_name]
-    x_train = data[features]
-    return features, x_train
+    X_train = data[features]
+    return features, X_train
 
 if __name__ == "__main__":
     print(alphas(start=-0.001,stop=0.002,step=0.001))
